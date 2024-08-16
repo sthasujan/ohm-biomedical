f04502ddca7507ffed206b1afff7459439856fad
Sujan Shrestha <sujan.spy007@gmail.com>
2024-08-16 11:02:02 +1000

    new changes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -38,13 +38,16 @@ const Products = () => {
 
   return (
     <div className='md:px-14 px-4 py-8 max-w-screen-2xl mx-auto'>
-      <div className='mt-15 mx-auto text-left w-full sm:w-11/12 md:w-4/5 p-6 bg-white shadow-lg rounded-lg flex flex-col md:flex-row items-center transition-all duration-300 ease-in-out'>
+      <div className='mt-0 mx-auto text-left w-full sm:w-11/12 md:w-4/5 p-6 bg-white shadow-lg rounded-lg flex flex-col md:flex-row items-center transition-all duration-300 ease-in-out'>
+        {/* Left Text Section */}
         <div className='md:w-1/2'>
-          <h2 className='text-xl text-black font-serif mb-3'>
+          <h2 className='text-base text-black font-serif mb-3'>
             OHM Biomedical is the exclusive distributor for Vyaire ventilators and CareFusion Bird blenders in Australia and New Zealand, providing expert sales and service. <br />
             <br />In addition to these specific products, we offer a wide range of biomedical services to leading healthcare companies in the industry.
           </h2>
         </div>
+
+        {/* Right Image Section */}
         <div className='md:w-1/2 flex justify-center md:justify-end'>
           <div className='w-40 h-40 md:w-60 md:h-60 lg:w-72 lg:h-72 xl:w-80 xl:h-80 bg-gray-200 rounded-md shadow-lg overflow-hidden'>
             <img
@@ -56,21 +59,24 @@ const Products = () => {
         </div>
       </div>
 
+      {/* Service Cards Section */}
       <div className='text-center my-8'>
-        {/* cards */}
+        <h2 className='text-4xl text-brandPrimary font-bold mb-2'>Our Products</h2>
         <div className='mt-14 grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 md:w-11/12 mx-auto gap-12'>
           {
             services.map(service => (
               <Link key={service.id} to={service.link} className='flex flex-col items-center'>
-                <div className='md-4 h-60 w-64 mx-auto rounded-tl-3xl rounded-br-3xl'>
-                  <img src={service.image} alt={service.title} className='-ml-5' />
+                <div className='mb-4 h-auto w-72 md:w-80 lg:w-96 xl:w-96 mx-auto rounded-xl shadow-2xl bg-white flex flex-col items-center p-4'>
+                  <img src={service.image} alt={service.title} className='w-full h-72 object-cover rounded-t-xl' />
+                  <div className='flex flex-col items-center'>
+                    <h4 className='text-xl font-bold text-neutralDGrey mb-2 mt-1'>{service.title}</h4>
+                    <ul className='text-sm text-neutralGrey flex flex-col items-start list-disc pl-5'>
+                      <li>{service.description}</li>
+                      <li>{service.description1}</li>
+                      <li>{service.description2}</li>
+                    </ul>
+                  </div>
                 </div>
-                <h4 className='text-2xl font-bold text-neutralDGrey mb-2 px-2'>{service.title}</h4>
-                <ul className='text-sm text-neutralGrey flex flex-col items-start list-disc pl-5'>
-                  <li>{service.description}</li>
-                  <li>{service.description1}</li>
-                  <li>{service.description2}</li>
-                </ul>
               </Link>
             ))
           }
